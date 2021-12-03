2693e8e794f086388aaeabedf022e89ffead93be
Samuel Kyletoft <samuel@kyletoft.se>
2021-12-03 02:12:05 +0100

    Dec 3 prep

---
+++ dec_3/shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+	# nativeBuildInputs is usually what you want -- tools you need to run
+	nativeBuildInputs = with pkgs; [
+		clojure
+		clojure-lsp
+		jdk
+		leiningen
+
+		neovim
+		nodejs
+		vimPlugins.coc-nvim
+	];
+	shellHook = ''
+		PS1="\e[32;1mnix-shell: \e[34m\w \[\033[00m\]\n↳ "
+		alias java_assertions="java -ea"
+		alias javac="javac -encoding UTF8"
+	'';
+}
---
+++ dec_3/src/dec_3/core.clj
@@ -0,0 +1,7 @@
+(ns dec-3.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
