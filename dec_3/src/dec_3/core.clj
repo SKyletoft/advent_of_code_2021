d689b73b275e6ae6069562dc483f0c5627d2a120
Samuel Kyletoft <samuel@kyletoft.se>
2021-12-03 15:08:06 +0100

    Day 3 parse (totally not just committing so I can switch computers)

--- dec_3/src/dec_3/core.clj
+++ dec_3/src/dec_3/core.clj
@@ -1,6 +1,16 @@
 (ns dec-3.core
   (:gen-class))
 
+(defn parse_number
+  [number]
+  [
+    bit-and number (bit-shift-left 1 0)
+    bit-and number (bit-shift-left 1 1)
+    bit-and number (bit-shift-left 1 2)
+    bit-and number (bit-shift-left 1 3)
+    bit-and number (bit-shift-left 1 4)
+  ])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
