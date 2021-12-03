1cf5f9775e4c06d4670d91a7eaec53ce87011e50
Samuel Kyletoft <samuel@kyletoft.se>
2021-12-04 00:54:40 +0100

    Day 3, part 2

--- dec_3/src/dec_3/core.clj
+++ dec_3/src/dec_3/core.clj
@@ -6,6 +6,10 @@
   [number]
   (if (not= number 0) 1 0))
 
+(defn long-bool-cast
+  [number]
+  (not= number 0))
+
 (defn parse_number_to_arr
   [number]
   (map
@@ -27,32 +31,46 @@
   [s]
   (clojure.string/split-lines s))
 
+(defn filter-lines
+  [bit input f]
+  (def input-length
+    (count input))
+  (if
+    (or
+      (= input-length 0)
+      (> bit 12))
+    nil
+    (if
+      (= input-length 1)
+      input
+      (let [sum (reduce + 0 (map (fn [line] (nth line bit)) input))
+            most-common-bit (if (f sum (/ input-length 2)) 1 0)
+            remaining-lines (filter
+              (fn [line] (= (nth line bit) most-common-bit))
+              input)]
+        (filter-lines (+ bit 1) remaining-lines f)))))
+
 (defn part1
   [input]
   (def step1 (map (comp parse_number_to_arr parse_bin) input))
   (def step2 (reduce (partial map +) [0 0 0 0 0 0 0 0 0 0 0 0] step1))
-  (def step3 (clojure.core/count step1))
+  (def step3 (count step1))
   (def step4 (map (partial < (/ step3 2)) step2))
-  (println step2)
   (def step5 (parse_arr_to_number step4))
   (* step5 (bit-and 2r111111111111 (bit-not step5))))
 
+(defn part2
+  [input]
+  (let [parsed_inputs (map (comp parse_number_to_arr parse_bin) input)
+        oxygen_bin (vec (first (filter-lines 0 parsed_inputs <)))
+        co2_bin (vec (first (filter-lines 0 parsed_inputs >=)))
+        oxygen (parse_arr_to_number (map long-bool-cast oxygen_bin))
+        co2 (parse_arr_to_number (map long-bool-cast co2_bin))]
+      (* oxygen co2)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (def input (line-seq (java.io.BufferedReader. *in*)))
-  (println (part1 input)))
+  (println (part1 input))
+  (println (part2 input)))
 
-(def test_case
-"00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010")
