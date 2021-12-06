8fe49464b85b6e9f60a7ab6f98a0f93dd3b3df3d
Samuel Kyletoft <samuel@kyletoft.se>
2021-12-06 14:57:53 +0100

    Day 6, parts 1 and 2

--- dec_6/dec_6.go
+++ dec_6/dec_6.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+	"fmt"
+)
+
+func simulate_day(fish *[]int) {
+	to_append := 0
+	for i := 0; i < len(*fish); i++ {
+		(*fish)[i]--
+		if (*fish)[i] == -1 {
+			(*fish)[i] = 6
+			to_append++
+		}
+	}
+	for i := 0; i < to_append; i++ {
+		*fish = append(*fish, 8)
+	}
+}
+
+func simulate_day_smart(fish *[9]int) {
+	tmp := (*fish)[0]
+	for i := 0; i < 8; i++ {
+		(*fish)[i] = (*fish)[i + 1]
+	}
+	(*fish)[6] += tmp
+	(*fish)[8] = tmp
+}
+
+func sum(fish [9]int) int {
+	sum := 0
+	for i := 0; i < 9; i++ {
+		sum += fish[i]
+	}
+	return sum
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	words := strings.Split(scanner.Text(), ",")
+	numbers := [9]int{0,0,0,0,0,0,0,0,0}
+	for i := 0 ; i < len(words); i++ {
+		res, _ := strconv.ParseInt(words[i], 0, 32)
+		numbers[res]++
+	}
+	for i := 0; i < 80; i++ {
+		simulate_day_smart(&numbers)
+	}
+	fmt.Println("Part 1: ", sum(numbers))
+	for i := 0; i < (256 - 80); i++ {
+		simulate_day_smart(&numbers)
+	}
+	fmt.Println("Part 2: ", sum(numbers))
 }
+
