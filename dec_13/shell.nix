2632a8451413ff8cf82412a89d78d2f37346a362
Samuel Kyletoft <samuel@kyletoft.se>
2021-12-18 00:53:00 +0100

    Day 13, Prep

---
+++ dec_13/shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+	# nativeBuildInputs is usually what you want -- tools you need to run
+	nativeBuildInputs = with pkgs; [
+		dotnet-sdk
+
+		neovim
+		nodejs
+		vimPlugins.coc-nvim
+	];
+	shellHook = ''
+		PS1="\e[32;1mnix-shell: \e[34m\w \[\033[00m\]\n↳ "
+		alias java_assertions="java -ea"
+		alias javac="javac -encoding UTF8"
+	'';
+}
